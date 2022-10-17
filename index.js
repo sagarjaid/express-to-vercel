191195835d62d2567909fb84f46e201b9201fec3
sagarjaid <69151227+sagarjaid@users.noreply.github.com>
2022-10-17 12:15:05 +0530

    new

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ const path = require('path');
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
-})
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Home Page Route'));
 
-app.listen(process.env.PORT || 3000);
+app.get('/about', (req, res) => res.send('About Page Route'));
+
+app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
+
+app.get('/contact', (req, res) => res.send('Contact Page Route'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
 
 module.exports = app;
