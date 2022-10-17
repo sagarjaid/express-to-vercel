5e0da59b44cc489835dcb6dba8a4ac0142fdf068
Sagar Jaid <sagarjaid321@gmail.com>
2022-10-17 12:54:54 +0530

    new route with api call

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const {videoService} = require('../service/videoService')
 
 router.use((req, res, next) => {
   console.log('Time: ', Date.now())
@@ -11,4 +12,11 @@ router.get("/", async (req, res) => {
   res.json({data: "hello"})
 });
 
+router.get('/video', (req, res) =>{videoService(req, res)});
+
 module.exports = router;
+
+
+// "unique-filename": "^2.0.1",
+// "fluent-ffmpeg": "^2.1.2",
+// "@ffmpeg-installer/ffmpeg": "^1.1.0"
---
+++ service/videoService.js
@@ -0,0 +1,80 @@
+const instagramGetUrl = require("../config/instagramGetUrl");
+
+// const ufileName = require("unique-filename");
+// const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+// const ffmpeg = require("fluent-ffmpeg");
+// ffmpeg.setFfmpegPath(ffmpegPath);
+// const path = require('path');
+// const fs = require('fs')
+// const https = require("https");
+
+const videoService = async (req, res)=>{
+
+    // const videoUrl =  req.query.url
+
+    const videoUrl = "https://www.instagram.com/reel/Ci8N8rEDQvL/"
+    
+    const igvideoUrl = await instagramGetUrl(videoUrl)
+    console.log(igvideoUrl, "igvideoUrl");
+    res.status(200).json(igvideoUrl)
+
+//     const igReelUrl = igvideoUrl.url_list[0]
+
+//     console.log(igReelUrl, "igReelUrl");
+
+//     // take a screenshort of the downloded video 
+
+//     const localPath = ufileName("/data", "igreel"); 
+
+//     console.log(localPath, "localPath");
+
+//   // download the video from URL
+
+//     https.get(igReelUrl, localPath, (res) => {
+
+//     const mp4filePath = "data" + localPath.slice(5) + ".mp4"
+
+//     console.log(mp4filePath, "mp4filePath");
+
+//     const writeStream = fs.createWriteStream(mp4filePath);
+ 
+//     res.pipe(writeStream);
+ 
+//     writeStream.on("finish", () => {
+//        writeStream.close();
+//        console.log("Download Completed!");
+//     })
+
+//     // take a screenshort of the downloded video 
+
+//     console.log("taking a screenshort....");
+
+//     new ffmpeg(igReelUrl).takeScreenshots({
+//       filename: localPath,
+//       count: 1,
+//       timemarks: ["1"],
+//       size: "3150x?"
+//       });
+
+//       console.log("screenshort taken")
+
+//       console.log(localPath, "localPath");
+
+//   })
+
+//     const mp4filePath = localPath.slice(7) + ".mp4"
+
+//     const reelfilePath = mp4filePath.toString()
+
+//     console.log(reelfilePath, "reelfilePath");
+
+//     const thFilePath = localPath.slice(7) + ".png"
+
+//     const pngfilePath = thFilePath.toString()
+
+//     console.log(pngfilePath, "pngfilePath");
+
+//     res.json({ igReelUrl: igReelUrl, thName: pngfilePath, reelName: reelfilePath})
+}
+
+module.exports = {videoService}
